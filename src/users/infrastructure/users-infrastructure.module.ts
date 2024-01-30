c31059d54f5325637046a0ef73b3c0b68b163f5b
Sergey Plotnikov <reaverz@yandex.ru>
2024-01-30 13:46:29 +0600

    Add stub OrmModule

--- src/users/infrastructure/users-infrastructure.module.ts
+++ src/users/infrastructure/users-infrastructure.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongoPersistenceModule } from './persistence/mongo/mongo-persistence.module';
+import { OrmPersistenceModule } from './persistence/orm/orm-persistence.module';
 
 @Module({})
 export class UsersInfrastructureModule {
   static use(driver: 'orm' | 'mongo') {
     const persistenceModule =
-      driver === 'mongo' ? MongoPersistenceModule : MongoPersistenceModule;
+      driver === 'mongo' ? MongoPersistenceModule : OrmPersistenceModule;
 
     return {
       module: UsersInfrastructureModule,
